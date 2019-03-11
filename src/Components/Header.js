6241e38c69a16a5a8d7051a977d80046e2f76b19
Brian <40767783+brishaw@users.noreply.github.com>
2019-03-11 14:21:30 -0400

    began styling of header componnent...

---
+++ src/Components/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './Style/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="top-bar">
+          <div className="container">
+            <div className="row">
+              <div className="top-bar-links">
+                <ul className="col-sm-6">
+                  <li className="top-bar-email">
+                    <i class="fas fa-envelope"></i>
+                    <a href="mailto:support@supportmail.com">supportmail.com</a>
+                  </li>
+                  <li className="top-bar-phone">
+                    <i className="fas fa-phone"></i>
+                    <span>1-919-999-0000</span>
+                  </li>
+                </ul>
+                <div className="col-sm-6 soc-icons text-right no-padding">
+                  <a href="#">
+                    <i className="fab fa-twitter-square"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-github-square"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-linkedin"></i>
+                  </a>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header;
