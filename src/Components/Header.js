b06b0ec3de6f926cc6a64f9625a9b91bd787a019
Brian <40767783+brishaw@users.noreply.github.com>
2019-03-12 16:09:55 -0400

    added header component with styles...

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Style/Header.css';
+import logo from './Images/logo.svg';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <div className="top-bar">
+        <div className="top-bar hidden-xs">
           <div className="container">
             <div className="row">
               <div className="top-bar-links">
@@ -29,12 +30,61 @@ class Header extends Component {
                   <a href="/">
                     <i className="fab fa-linkedin"></i>
                   </a>
-
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </div> {/* // top-bar */}
+        <nav className="navbar navbar-static-top">
+          <div className="navigation">
+            <div className="container relative">
+              <form method="get" className="search-wrap">
+                <input type="search" className="form-control" placeholder="Type &amp; Hit Enter" />
+              </form>
+              <div className="row flex">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                </div>
+                <div className="logo-container">
+                  <div className="logo-wrap">
+                    <a href="/">
+                      <img className="logo" src={logo} alt="logo" />
+                    </a>
+                  </div>
+                </div>
+                <div className="col-md-9 nav-wrap flex">
+                  <div className="collapse navbar-collapse" id="navbar-collapse">
+                    <ul className="nav navbar-nav navbar-right">
+                      <li className="dropdown active">
+                        <a href="/" className="dropdown-toggle">Home</a>
+                      </li>
+                      <li className="dropdown">
+                        <a href="/" className="dropdown-toggle">Portfolio</a>
+                      </li>
+                      <li className="dropdown">
+                        <a href="/" className="dropdown-toggle">About</a>
+                      </li>
+                      <li className="dropdown">
+                        <a href="/" className="dropdown-toggle">Contact</a>
+                      </li>
+                      <li>
+                        <a href="/" className="nav-search">
+                          <i className="fa fa-search search-trigger"></i>
+                          <i className="fa fa-times search-close"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav> {/* // nav */}
       </header>
     )
   }
