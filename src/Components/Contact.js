73d92b13e040556792e2cd5841b5106dd9f55ba9
Brian <40767783+brishaw@users.noreply.github.com>
2019-03-25 20:20:44 -0400

    more styling...

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
     return (
       <section className="contact">
         <section className="pane">
-          <article>
+          <article className="frame">
             <p>Phasellus quis nunc at augue mollis luctus. Vivamus molestie massa quis tortor suscipit viverra. Vivamus odio nisi, tempor et pulvinar tempus, rutrum sit amet libero. Quisque dignissim egestas metus, hendrerit fermentum justo consectetur vitae. Sed feugiat egestas convallis. Mauris quis egestas lorem. Nulla aliquam tortor ac quam faucibus laoreet. Vivamus gravida dui sed porta accumsan. Sed gravida metus risus, ut ornare justo commodo vel. In vitae lectus molestie, luctus metus id, commodo diam. Aenean at fermentum diam, posuere lacinia tellus. Ut elit massa, accumsan id bibendum sed, dictum sit amet erat. Phasellus non consequat risus.</p>
           </article>
         </section>
